3228c993a515694b299fb67387b824326503cf89
SS <ssdevelop@yandex.ru>
2013-07-29 11:45:53 +0300

    ticket:1. done

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,8 +1,10 @@
 from annoying.decorators import render_to
 from django.contrib.auth.models import User
+from models import Contact
 
 
 @render_to('hello/home.html')
 def home(request):
     users = User.objects.filter()
-    return {'users': users}
+    contact = Contact.objects.get(pk=1)
+    return {'users': users, 'contact': contact}
