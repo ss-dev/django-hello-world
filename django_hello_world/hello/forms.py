bd6ce9fbc078c0dd650695540095c1516b902c2a
SS <ssdevelop@yandex.ru>
2013-07-31 10:30:23 +0300

    ticket:6. add date-widget for date_birth field

--- django_hello_world/hello/forms.py
+++ django_hello_world/hello/forms.py
@@ -1,7 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateField
+from django.forms.extras.widgets import SelectDateWidget
 from django_hello_world.hello.models import Contact
 
 
+BIRTH_YEAR_CHOICES = ('1981', '1982', '1983', '1984', '1985')
+
+
 class ContactForm(ModelForm):
+    date_birth = DateField(widget=SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+
     class Meta:
         model = Contact
