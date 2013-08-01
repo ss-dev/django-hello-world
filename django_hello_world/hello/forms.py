06a9b3d2ce3a360885e4045b1a5590576499cd28
SS <ssdevelop@yandex.ru>
2013-08-01 09:27:05 +0300

    ticket:6. fix datepicker format

--- django_hello_world/hello/forms.py
+++ django_hello_world/hello/forms.py
@@ -5,7 +5,8 @@ from django_hello_world.hello.widgets import DatePickerWidget
 
 class ContactForm(ModelForm):
     date_birth = DateField(widget=DatePickerWidget(
-        params="changeMonth: true, changeYear: true, dateFormat: 'yy.mm.dd', yearRange: 'c-40:c+1'",
+        params="""changeMonth: true, changeYear: true,
+            dateFormat: 'yy-mm-dd', yearRange: 'c-40:c+1'""",
         attrs={'class': 'datepicker', }
     ))
 
